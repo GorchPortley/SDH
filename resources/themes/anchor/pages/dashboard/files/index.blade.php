5c95b90df2c0b2e7fd480267404713e1ace3b2ab
GorchPortley <admin@sdlabs.cc>
2024-11-21 13:51:11 -0500

    Added file manager page and dashboard link
    https://github.com/UniSharp/laravel-filemanager
    removed shared folder for now.

--- resources/themes/anchor/pages/dashboard/files/index.blade.php
+++ resources/themes/anchor/pages/dashboard/files/index.blade.php
@@ -1,3 +1,13 @@
+<?php
+
+use function Laravel\Folio\{middleware, name};
+
+middleware('auth');
+name('dashboard.files');
+?>
+
 <x-layouts.app>
-    
+    @volt('files')
+    <iframe src="/laravel-filemanager" class="rounded-lg" style="width: 100%; height: 850px; overflow: hidden;"></iframe>
+    @endvolt
 </x-layouts.app>
