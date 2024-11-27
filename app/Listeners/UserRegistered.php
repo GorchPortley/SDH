6ae2a92c026b5c74a4d75b0d61d372a262b5e898
GorchPortley <admin@sdlabs.cc>
2024-11-27 15:04:37 -0500

    sso stuff

--- app/Listeners/UserRegistered.php
+++ app/Listeners/UserRegistered.php
@@ -5,12 +5,16 @@ namespace App\Listeners;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Maicol07\SSO\Flarum;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserRegistered
 {
     /**
      * Create the event listener.
      */
+
     public function __construct()
     {
         //
@@ -21,7 +25,12 @@ class UserRegistered
      */
     public function handle(Registered $event): void
     {
-        // $user = $event->user;
-        // Perform any functionality to the user here...
+        $user = $event->user;
+        $flarum = new Flarum($options);
+        $user = $flarum->user($username)
+        $user_alias = $flarum->user();
+        $flarum_user->attributes->email = 'user@example.com';
+        $flarum_user->attributes->password = 'userpassword';
+        $flarum_user->register();
     }
 }
